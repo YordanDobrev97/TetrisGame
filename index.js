87e2ace1fba9cbff001b575e3aed60a4ecec5335
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-10-12 20:46:24 +0300

    Automatic fall down of canvas object

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 
-function play() {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'red';
-    const figureObj = {x: 50, y: 50, width: 30, height: 30};
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'red';
+const figureObj = {x: 50, y: 50, width: 30, height: 30};
 
+function play() {
     ctx.clearRect(0,0,canvas.width,canvas.height);
     ctx.fillRect(figureObj.x, figureObj.y, figureObj.width, figureObj.height);
 
     const defaultValue = 3;
+    figureObj.y += defaultValue;
     document.onkeydown = (e) => {
         if (e.keyCode === 37) {
             figureObj.x -= defaultValue;
@@ -22,48 +23,9 @@ function play() {
         else if (e.keyCode === 40) {
             figureObj.y += defaultValue;
         }
-
         ctx.clearRect(0,0,canvas.width,canvas.height);
         ctx.fillRect(figureObj.x, figureObj.y, figureObj.width, figureObj.height);
     };
-
-    console.log('yes babayy');
 };
 
-play();
-
-
-
-
-
-
-
-
-
-
-
-
-//moveFigureWithKeyWord();
-// function moveFigureWithKeyWord() {
-//     const figureObj = {x: 50, y: 50, width: 30, height: 30};
-//     ctx.fillRect(figureObj.x, figureObj.y, figureObj.width, figureObj.height);
-
-//     const defaultValue = 3;
-//     document.onkeydown = (e) => {
-//         if (e.keyCode === 37) {
-//             figureObj.x -= defaultValue;
-//         }
-//         else if (e.keyCode === 38) {
-//             figureObj.y -= defaultValue;
-//         }
-//         else if (e.keyCode === 39) {
-//             figureObj.x += defaultValue;
-//         }
-//         else if (e.keyCode === 40) {
-//             figureObj.y += defaultValue;
-//         }
-
-//         ctx.clearRect(0,0,canvas.width,canvas.height);
-//         ctx.fillRect(figureObj.x, figureObj.y, figureObj.width, figureObj.height);
-//     };
-// }
+setInterval(play, 300);
